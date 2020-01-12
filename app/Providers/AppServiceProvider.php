8972c06fbc5f914fa0bf4a6ba8edccdb289b9700
John0079 <270752265@qq.com>
2020-01-12 16:02:58 +0800

    亲测成功运行

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\ServiceProvider;
+use Laravel\Passport\Passport;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+        Passport::tokensCan([
+            'test1' => 'for test1',
+            'test2' => 'for test2',
+        ]);
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+        Schema::defaultStringLength(191);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::post('/oauth/token', '\Laravel\Passport\Http\Controllers\AccessTokenController@issueToken');
+
+
+Route::post('/register', 'PassportController@register');
+Route::post('/login', 'PassportController@login');
+Route::post('/refresh', 'PassportController@refresh');
+Route::post('/logout', 'PassportController@logout');
+
+//
+//Route::get('test', function () {
+//    return 'ok';
+//})->middleware('auth');
+
+//
+//Route::get('test', function () {
+//    return 'ok';
+//})->middleware('scopes:test2');
+
+
+Route::get('test', function () {
+    if(auth()->user()->tokenCan('test1')){
+        return "123";
+    }
+})->middleware('auth');
+
+
+Route::post('tt', function () {
+    return 'ok';
+});
